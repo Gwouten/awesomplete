481142676ab40304e14337d2d32d9f5ff1b5ffa9
Wouter <wouter.deloof@gmail.com>
2018-04-24 16:06:30 +0200

    explored creating lists for autocompleting

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
+// Instantiate Awesomplete
+const input = document.querySelector("input");
+const list = [
+  {
+    code: 1000,
+    commune: "Bruxelles",
+    candidats: ["Olivier", "Wouter"]
+  },
+  {
+    code: 1050,
+    commune: "Ixelles",
+    candidats: ["Gilles", "Christophe"]
+  },
+  {
+    code: 3000,
+    commune: "Leuven",
+    candidats: ["Etienne", "Sandro"]
+  }
+];
+const myList = list.map(function(item) {});
 
+new Awesomplete(input, {
+  list: myList,
+  minChars: 1,
+  replace: function(text) {
+    this.input.value = text + " tadaa";
+  }
+});
+
+input.addEventListener("awesomplete-select", function() {
+  console.log("submitted!");
+});
